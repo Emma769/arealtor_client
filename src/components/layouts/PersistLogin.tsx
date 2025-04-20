6a2178b189f0b071472250a835a74d7db6f74eb6
huey <emma769ose@gmail.com>
2025-04-20 13:02:10 +0100

    made some more auth adjustments

--- src/components/layouts/PersistLogin.tsx
+++ src/components/layouts/PersistLogin.tsx
@@ -14,7 +14,6 @@ export default function PersistLogin() {
 
     const verifyLogin = async () => {
       try {
-        console.log("ran verify login");
         await refreshfn();
       } catch (err) {
         console.error(err);
